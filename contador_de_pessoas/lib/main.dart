4720a4116e3a5dca722b445e244c81b9df5ea462
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-02 03:08:46 -0300

    Criação de botõs, linhas e hierarquia

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -10,9 +10,29 @@ void main() {
             "Pessoas: 0",
             style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
           Text(
             "Pode Entrar!",
-            style: TextStyle(color: Colors.white,
+            style: TextStyle(
+                color: Colors.white,
                 fontStyle: FontStyle.italic,
                 fontSize: 30.0),
           )
