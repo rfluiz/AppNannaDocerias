4e1d4fcf133fc411bc9df8cdf0372bda9febba8a
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-03 02:48:57 -0300

    Construindo com FutureBuilder

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -6,17 +6,15 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
 
-const request ='https://api.hgbrasil.com/finance?format=json&key=3123433d';
+const request = 'https://api.hgbrasil.com/finance?format=json&key=3123433d';
 
 void main() async {
-
   runApp(MaterialApp(
-    home:Home(),
-  )
-  );
+    home: Home(),
+  ));
 }
 
-Future<Map> getData() async{
+Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
 }
@@ -35,6 +33,32 @@ class _HomeState extends State<Home> {
         title: Text("Conversor de Moedas"),
         backgroundColor: Colors.amber,
         centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                  child: Text(
+                "Carregando Dados...",
+                style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                textAlign: TextAlign.center,)
+              );
+            default:
+              if(snapshot.hasError){
+                Center(
+                    child: Text(
+                      "Erro ao carregar os dados :(",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center,)
+                );
+              }else{
+                return Container(color: Colors.green,);
+              }
+          }
+        },
       ),
     );
   }
