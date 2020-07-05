5d60b01ae12ed133adbf03c7d31d5dbf477c4b50
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-05 02:59:48 -0300

    Criando Lista

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -48,6 +48,23 @@ class _HomeState extends State<Home> {
                 )
               ],
             ),
+          ),
+          Expanded(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: (context, index){
+                  return CheckboxListTile(
+                    title: Text(_toDoList[index] ["title"]),
+                    value: _toDoList[index]["ok"],
+                    secondary: CircleAvatar(
+                      child: Icon(
+                        _toDoList[index]["ok"] ?
+                            Icons.check : Icons.error
+                      ),
+                    ),
+                  );
+                }),
           )
         ],
       ),
