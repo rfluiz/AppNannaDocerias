6c09116a5ddda94f4b6d531e835f1e8bb6fc1f8c
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-05 03:24:13 -0300

    Salvando as tarefas permanentemente

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -20,6 +20,16 @@ class _HomeState extends State<Home> {
 
   List _toDoList = [];
 
+  @override
+  void initState(){
+    super.initState();
+    _readData().then((data){
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
   void _addToDo(){
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -27,6 +37,7 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+      _saveData();
     });
   }
 
@@ -79,6 +90,7 @@ class _HomeState extends State<Home> {
                     onChanged: (c){
                       setState(() {
                         _toDoList[index]["ok"] = c;
+                        _saveData();
                       });
                     },
                   );
