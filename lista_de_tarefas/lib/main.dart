0553c39986fd164a28897b416c235ecf948af67f
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-05 03:14:53 -0300

    Adicionando itens na lista

--- lista_de_tarefas/lib/main.dart
+++ lista_de_tarefas/lib/main.dart
@@ -16,7 +16,19 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  final _toDoController = TextEditingController();
+
   List _toDoList = [];
+
+  void _addToDo(){
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = _toDoController.text;
+      _toDoController.text = "";
+      newToDo["ok"] = false;
+      _toDoList.add(newToDo);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +46,7 @@ class _HomeState extends State<Home> {
               children: <Widget>[
                 Expanded(
                   child: TextField(
+                      controller: _toDoController,
                       decoration: InputDecoration(
                           labelText: "Nova Tarefa",
                           labelStyle: TextStyle(color: Colors.blueAccent)
@@ -44,7 +57,7 @@ class _HomeState extends State<Home> {
                   color: Colors.blueAccent,
                   child: Text("Add"),
                   textColor: Colors.white,
-                  onPressed: (){},
+                  onPressed: _addToDo,
                 )
               ],
             ),
@@ -63,6 +76,11 @@ class _HomeState extends State<Home> {
                             Icons.check : Icons.error
                       ),
                     ),
+                    onChanged: (c){
+                      setState(() {
+                        _toDoList[index]["ok"] = c;
+                      });
+                    },
                   );
                 }),
           )
