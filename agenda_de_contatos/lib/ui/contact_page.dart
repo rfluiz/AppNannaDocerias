e5fd7d1f6db0baace501a9698017b3bfd5c00048
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-17 20:44:54 -0300

    Criando tela de contato

---
+++ agenda_de_contatos/lib/ui/contact_page.dart
@@ -0,0 +1,45 @@
+import 'package:agenda_de_contatos/helpers/contact_helper.dart';
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+
+  final Contact contact;
+
+  ContactPage({this.contact});
+
+  @override
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+
+  Contact _editContact;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    if(widget.contact == null){
+      _editContact = Contact();
+    }else{
+      _editContact = Contact.fromMap(widget.contact.toMap());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(_editContact.name ?? "Novo Contato"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.save),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+}
