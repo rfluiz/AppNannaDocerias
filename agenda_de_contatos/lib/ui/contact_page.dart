18d9814fb987d962db049261200d54505ee44ae1
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-17 22:29:38 -0300

    Integração com o telefone e a camera

--- agenda_de_contatos/lib/ui/contact_page.dart
+++ agenda_de_contatos/lib/ui/contact_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:agenda_de_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
 
@@ -79,6 +80,14 @@ class _ContactPageState extends State<ContactPage> {
                       )
                   ),
                 ),
+                onTap: (){
+                  ImagePicker.pickImage(source: ImageSource.camera).then((file){
+                    if(file == null) return;
+                    setState(() {
+                      _editContact.img = file.path;
+                    });
+                  });
+                },
               ),
               TextField(
                 controller: _nameController,
--- agenda_de_contatos/lib/ui/home_page.dart
+++ agenda_de_contatos/lib/ui/home_page.dart
@@ -4,6 +4,7 @@ import 'package:agenda_de_contatos/helpers/contact_helper.dart';
 import 'package:agenda_de_contatos/ui/contact_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -113,7 +114,8 @@ class _HomePageState extends State<HomePage> {
                           style: TextStyle(color: Colors.red, fontSize: 20.0),
                         ),
                         onPressed: (){
-
+                          launch("tel:${contacts[index].phone}");
+                          Navigator.pop(context);
                         },
                       ),
                     ),
