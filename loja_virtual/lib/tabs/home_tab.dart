98f2aca251852813f7a17e3c1d233c2bdf9b88ef
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-27 18:40:11 -0300

    Obtendo as imagens da tela inicial

--- loja_virtual/lib/tabs/home_tab.dart
+++ loja_virtual/lib/tabs/home_tab.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class HomeTab extends StatelessWidget {
@@ -32,6 +34,31 @@ class HomeTab extends StatelessWidget {
                 title: const Text("Novidades"),
                 centerTitle: true,
               ),
+            ),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance.collection("home").orderBy("pos").getDocuments(),
+              builder: (context, snapshot){
+                if(!snapshot.hasData)
+                  return SliverToBoxAdapter(
+                    child: Container(
+                     height: 200.0,
+                     alignment: Alignment.center,
+                     child: CircularProgressIndicator(
+                       valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+                     ),
+                    )
+                  );
+                else{
+                  print(snapshot.data.documents.length);
+                  return SliverToBoxAdapter(
+                      child: Container(
+                        height: 200.0,
+                        alignment: Alignment.center,
+                        child: Container()
+                      ),
+                  );
+                }
+              },
             )
           ],
         )
