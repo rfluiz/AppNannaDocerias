77c8ccfdb4afe02cfd3c3274c8b7a7b29ff32254
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-27 18:52:28 -0300

    Grade de Imagens

--- loja_virtual/lib/tabs/home_tab.dart
+++ loja_virtual/lib/tabs/home_tab.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 
 class HomeTab extends StatelessWidget {
@@ -31,7 +33,7 @@ class HomeTab extends StatelessWidget {
               backgroundColor: Colors.transparent,
               elevation: 0.0,
               flexibleSpace: FlexibleSpaceBar(
-                title: const Text("Novidades"),
+                title: const Text("Bazante Doces App"),
                 centerTitle: true,
               ),
             ),
@@ -49,14 +51,23 @@ class HomeTab extends StatelessWidget {
                     )
                   );
                 else{
-                  print(snapshot.data.documents.length);
-                  return SliverToBoxAdapter(
-                      child: Container(
-                        height: 200.0,
-                        alignment: Alignment.center,
-                        child: Container()
-                      ),
-                  );
+                 return SliverStaggeredGrid.count(
+                   crossAxisCount: 2,
+                   mainAxisSpacing: 1.0,
+                   crossAxisSpacing: 1.0,
+                   staggeredTiles: snapshot.data.documents.map((doc){
+                     return StaggeredTile.count(doc.data["x"], doc.data["y"]);
+                   }
+                   ).toList(),
+                   children: snapshot.data.documents.map((doc){
+                     return FadeInImage.memoryNetwork(
+                         placeholder: kTransparentImage,
+                         image: doc.data["image"],
+                       fit: BoxFit.cover,
+                     );
+                   }
+                   ).toList(),
+                 );
                 }
               },
             )
