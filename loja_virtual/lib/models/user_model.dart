6c17602a6c86cf38cab4e1eb33471fece24e1656
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-28 06:55:13 -0300

    Realizando o login

--- loja_virtual/lib/models/user_model.dart
+++ loja_virtual/lib/models/user_model.dart
@@ -13,6 +13,13 @@ class UserModel extends Model {
 
   bool isLoading = false;
 
+  @override
+  void addListener(VoidCallback listener) {
+    super.addListener(listener);
+
+    _loadCurrentUser();
+  }
+
   void signUp({@required Map<String, dynamic> userData, @required String pass,
     @required VoidCallback onSuccess, @required VoidCallback onFail}){
     isLoading = true;
@@ -37,14 +44,25 @@ class UserModel extends Model {
         });
   }
 
-  void signIn() async{
+  void signIn({@required String email, @required String pass,
+    @required VoidCallback onSuccess, @required VoidCallback onFail}) async{
     isLoading = true;
     notifyListeners();
 
-  await Future.delayed(Duration(seconds: 3));
+    _auth.signInWithEmailAndPassword(email: email, password: pass).then((user) async{
+      firebaseUser =user;
 
-  isLoading = false;
-  notifyListeners();
+      await _loadCurrentUser();
+
+      onSuccess();
+      isLoading = false;
+      notifyListeners();
+
+    }).catchError((e){
+      onFail();
+      isLoading = false;
+      notifyListeners();
+    });
   }
   void recoverPass(){
 
@@ -66,6 +84,19 @@ class UserModel extends Model {
     this.userData = userData;
     await Firestore.instance.collection('users').document(firebaseUser.uid).setData(userData);
 
+  }
+
+  Future<Null> _loadCurrentUser() async {
+    if(firebaseUser == null){
+      firebaseUser = await _auth.currentUser();
+    }
+    if(firebaseUser != null){
+      if(userData["name"] == null){
+        DocumentSnapshot docUser = await Firestore.instance.collection("users").document(firebaseUser.uid).get();
+        userData = docUser.data;
+      }
+    }
+    notifyListeners();
   }
 
 }
--- loja_virtual/lib/screens/login_screen.dart
+++ loja_virtual/lib/screens/login_screen.dart
@@ -4,100 +4,131 @@ import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/signup_screen.dart';
 import 'package:scoped_model/scoped_model.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
+  final _scafoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Entrar"),
-        centerTitle: true,
-        actions: <Widget>[
-          FlatButton(
-            child: Text(
-              "Criar Conta",
-              style: TextStyle(
-                fontSize: 15.0,
+        key: _scafoldKey,
+        appBar: AppBar(
+          title: Text("Entrar"),
+          centerTitle: true,
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                "Criar Conta",
+                style: TextStyle(
+                  fontSize: 15.0,
+                ),
               ),
-            ),
-            textColor: Colors.white,
-            onPressed: (){
-              Navigator.of(context).pushReplacement(
-                MaterialPageRoute(builder: (context)=>SignUpScreen())
+              textColor: Colors.white,
+              onPressed: (){
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context)=>SignUpScreen())
+                );
+              },
+            )
+          ],
+        ),
+        body: ScopedModelDescendant<UserModel>(
+          builder: (context, child, model){
+            if(model.isLoading){
+              return Center(
+                child: CircularProgressIndicator(),
               );
-            },
-          )
-        ],
-      ),
-      body: ScopedModelDescendant<UserModel>(
-        builder: (context, child, model){
-          if(model.isLoading){
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return Form(
-            key: _formKey,
-            child: ListView(
-              padding: EdgeInsets.all(16.0),
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "E-mail"
-                  ),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (text){
-                    if(text.isEmpty || !text.contains("@")) return "E-mail inválido!";
-                  },
-                ),
-                SizedBox(height: 16.0),
-                TextFormField(
-                  decoration: InputDecoration(
-                      hintText: "Senha"
-                  ),
-                  obscureText: true,
-                  validator: (text){
-                    if(text.isEmpty || text.length < 6) return "Senha inválida!";
-                  },
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: FlatButton(
-                    onPressed: (){
-
-                    },
-                    child:
-                    Text("Esqueci minha senha", textAlign: TextAlign.right,),
-                    padding: EdgeInsets.zero,
-                  ),
-                ),
-                SizedBox(height: 16.0,),
-                SizedBox(
-                  height: 44.0,
-                  child: RaisedButton(
-                    child: Text("Entrar",
-                      style: TextStyle(
-                          fontSize: 18.0
-                      ),
+            }
+            return Form(
+              key: _formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16.0),
+                children: <Widget>[
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                        hintText: "E-mail"
                     ),
-                    textColor: Colors.white,
-                    color: Theme.of(context).primaryColor,
-                    onPressed: (){
-                      if(_formKey.currentState.validate()){
-
-                      }
-
-                      model.signIn();
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (text){
+                      if(text.isEmpty || !text.contains("@")) return "E-mail inválido!";
                     },
                   ),
-                ),
-              ],
-            ),
-          );
-        },
-      )
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    controller: _passController,
+                    decoration: InputDecoration(
+                        hintText: "Senha"
+                    ),
+                    obscureText: true,
+                    validator: (text){
+                      if(text.isEmpty || text.length < 6) return "Senha inválida!";
+                    },
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: FlatButton(
+                      onPressed: (){
+
+                      },
+                      child:
+                      Text("Esqueci minha senha", textAlign: TextAlign.right,),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
+                  SizedBox(height: 16.0,),
+                  SizedBox(
+                    height: 44.0,
+                    child: RaisedButton(
+                      child: Text("Entrar",
+                        style: TextStyle(
+                            fontSize: 18.0
+                        ),
+                      ),
+                      textColor: Colors.white,
+                      color: Theme.of(context).primaryColor,
+                      onPressed: (){
+                        if(_formKey.currentState.validate()){
+
+                        }
+
+                        model.signIn(
+                          email: _emailController.text,
+                          pass: _passController.text,
+                          onSuccess: _onSuccess,
+                          onFail: _onFail,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        )
+    );
+  }
+  void _onSuccess(){
+    Navigator.of(context).pop();
+  }
+
+  void _onFail(){
+    _scafoldKey.currentState.showSnackBar(
+        SnackBar(content: Text("Falha ao realizar login!"),
+          backgroundColor: Colors.red,
+          duration: Duration(seconds: 3),
+        )
     );
   }
 }
+
+
