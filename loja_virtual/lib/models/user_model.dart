905a9051a8cdd79d902300fa5c967b9e62a71dbe
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-28 03:35:48 -0300

    Código para criar usuário

--- loja_virtual/lib/models/user_model.dart
+++ loja_virtual/lib/models/user_model.dart
@@ -1,13 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class UserModel extends Model {
 
+  FirebaseAuth _auth = FirebaseAuth.instance;
 
+  FirebaseUser firebaseUser;
+  Map<String, dynamic> userData = Map();
 
   bool isLoading = false;
 
-  void signUp(){
+  void signUp({@required Map<String, dynamic> userData, @required String pass,
+    @required VoidCallback onSuccess, @required VoidCallback onFailed}){
+    isLoading = true;
+    notifyListeners();
+    
+    _auth.createUserWithEmailAndPassword(
+        email: userData["email"], 
+        password: pass).then((user) async{
+          firebaseUser = user;
 
+          await _saveUserData(userData);
+
+          onSuccess();
+          isLoading = false;
+          notifyListeners();
+
+        }).catchError((e){
+          onFailed();
+
+          isLoading = false;
+          notifyListeners();
+        });
   }
 
   void signIn() async{
@@ -21,6 +47,11 @@ class UserModel extends Model {
   }
   void recoverPass(){
 
+  }
+
+  Future<Null> _saveUserData(Map<String, dynamic> userData) async{
+    this.userData = userData;
+    await Firestore.instance.collection("users").document(firebaseUser.uid).setData(userData);
   }
 
 }
