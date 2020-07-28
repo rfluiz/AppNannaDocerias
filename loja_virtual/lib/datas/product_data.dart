b80203c70aac38d5e6ce7cf1ab26eb7ecfadc6c4
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-27 23:55:14 -0300

    Estruturando os dados dos produtos

---
+++ loja_virtual/lib/datas/product_data.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ProductData{
+
+  String category;
+  String id;
+  String title;
+  String description;
+
+  double price;
+
+  List images;
+  List sizes;
+
+  ProductData.fromDocument(DocumentSnapshot snapshot){
+    id = snapshot.documentID;
+    title = snapshot.data["title"];
+    description = snapshot.data["description"];
+    price = snapshot.data["price"];
+    images = snapshot.data["images"];
+    title = snapshot.data["title"];
+    sizes = snapshot.data["size"];
+  }
+
+}
