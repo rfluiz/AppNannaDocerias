bcc4247a10ef0393ef674c6781dac47640a0b11d
Gabriel Bazante <gabrielbazante7@gmail.com>
2020-07-29 20:14:04 -0300

    Criando o corpo da tela do carrinho

--- loja_virtual/lib/screens/cart_screen.dart
+++ loja_virtual/lib/screens/cart_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
+import 'package:loja_virtual/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
+
+import 'login_screen.dart';
 
 class CartScreen extends StatelessWidget {
   @override
@@ -21,12 +24,69 @@ class CartScreen extends StatelessWidget {
                   "${p ?? 0} ${p == 1 ? "ITEM" : "ITENS"}",
                   style: TextStyle(fontSize: 17.0),
                 );
-
               },
             ),
           )
         ],
       ),
+      body:ScopedModelDescendant<CartModel>(
+        builder: (context, child, model){
+          if(model.isLoading && UserModel.of(context).isLoggedIn()){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }else if(!UserModel.of(context).isLoggedIn()){
+            return Container(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.remove_shopping_cart,
+                  size: 80.0,
+                  color: Theme.of(context).primaryColor,
+                  ),
+                  Text(
+                    "Faça o login para adicionar produtos!",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16.0),
+                  RaisedButton(
+                    child: Text(
+                      "Entrar",
+                      style: TextStyle(
+                        fontSize: 18.0
+                      ),
+                    ),
+                    textColor: Colors.white,
+                    color: Theme.of(context).primaryColor ,
+                    onPressed: (){
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context)=> LoginScreen())
+                      );
+                    },
+                  )
+                ],
+              ),
+            );
+          }else if (model.products == null || model.products.length == 0){
+            return Center(
+              child: Text(
+                "Nenhum produto no carrinho",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+        }
+      )
     );
   }
 }
